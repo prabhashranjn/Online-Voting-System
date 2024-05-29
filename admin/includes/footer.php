c20bcce7fe0ac5cdccebd6cf18664604716cfff2
prabhashranjn <prabhashranjan895@gmail.com>
2024-05-30 00:44:51 +0530

    admin file added

---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+<!--style="background-color: #717A83 "  -->
+<!-- style="background: linear-gradient(135deg, #FF9933, #FFFFFF, #138808); font-size:15px; color:black ; " -->
+<footer class="main-footer" style="background-color: #90EE90 " >
+    <div class="pull-right hidden-xs "  style="color:black ; font-size: 13px"  >
+      <b>All rights reserved</b>
+    </div  >
+    <b style="color:black ; font-size: 13px"><strong>Copyright &copy; PKR </strong></b>
+</footer>
